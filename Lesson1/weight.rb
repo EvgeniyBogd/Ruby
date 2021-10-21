50fb1fa4842f0054d5db543df133dc6d8f058475
EvgeniyBogd <89639797+EvgeniyBogd@users.noreply.github.com>
2021-10-21 23:46:57 +0500

    Add files via upload

--- Lesson1/weight.rb
+++ Lesson1/weight.rb
@@ -1,1 +1,15 @@
+puts "Как тебя зовут?"
+name = gets.chomp
 
+puts "Какой у тебя рост?"
+height = gets.chomp
+
+
+
+weight = (height.to_i - 110) * 1.15
+
+if weight >=0
+  puts "#{name}, привет! Твой идеальный вес #{weight}!" 
+else
+  puts "#{name}, привет! Ваш вес уже оптимален!" 
+end
